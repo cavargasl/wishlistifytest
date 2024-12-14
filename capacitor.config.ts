64e4f639f65270d8c683bcfa1c9ef787cf777c1c
Camilo Vargas <camilovargasingm@gmail.com>
2024-12-14 12:56:41 -0500

    get started

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.wishlistifytest',
+  appName: 'wishlistifytest',
+  webDir: 'dist'
+};
+
+export default config;
