3bcae7b7717f1298b17bd431708f40eae6f3ac68
Camilo Vargas <camilovargasingm@gmail.com>
2024-12-14 18:13:24 -0500

    feat: install and configure react query for caching

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
