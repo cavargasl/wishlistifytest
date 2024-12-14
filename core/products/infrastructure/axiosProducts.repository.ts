a92e54c2080a32e6e7b39d64857e8bbf29f14f7b
Camilo Vargas <camilovargasingm@gmail.com>
2024-12-14 13:27:52 -0500

    feat: create implementation of products repository

---
+++ core/products/infrastructure/axiosProducts.repository.ts
@@ -0,0 +1,25 @@
+
+
+import axiosInstance from "@core/shared/api/axiosConfig";
+import { ProductRepository } from "../domain/productRepository";
+import { productsAdapter, productAdapter } from "./adapter";
+import { ProductDTO } from "./dto";
+
+export const axiosProducts = (): ProductRepository => ({
+  getProducts: async (params) => {
+    const products = await axiosInstance.get<ProductDTO[]>('/products', {
+      params: {
+        ...params,
+        offset: params?.pagination?.offset || 0,
+        limit: params?.pagination?.limit || 5,
+      },
+    });
+    return productsAdapter(products.data);
+  },
+
+  getProductById: async (productId: number) => {
+    const product = await axiosInstance.get<ProductDTO>(`/products/${productId}`);
+    return productAdapter(product.data);
+  }
+})
+    
