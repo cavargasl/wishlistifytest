b29eb627152fa583dad124caa3d87a9a1c9fe250
Camilo Vargas <camilovargasingm@gmail.com>
2024-12-14 13:21:50 -0500

    conf: add absolute route import

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@core': '/core',
+    },
+  },
 })
